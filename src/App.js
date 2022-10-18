1e3b2706615511cf3fecba3767b6c90a2d90ad89
Emiliano Rotta <emilianorottaf@gmail.com>
2022-10-18 11:49:06 -0300

    Parte 1 del ejercicio, sin servicios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import TodoList from './components/TodoList.js';
+import AddTodo from './components/AddTodo.js';
+
+import EditTodo from "./components/EditTodo"
+
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [edit, setEdit] = useState(false);//2
+  const [editTodo, setEditTodo] = useState(false);//2
+
+  function onDelete(id) {
+    const newTodos = todos.filter(todo => todo.id !== id);
+    setTodos(newTodos);
+  }
+
+  function onSubmit(e) {
+    e.preventDefault();
+    const name = e.target.name.value;
+    setTodos(prev => [...prev, { id: prev.length + 1, name}]);
+    e.target.reset();
+  }
+
+  function onEdit(todo) { //2
+    setEdit(true);
+    setEditTodo(todo);
+  }
+
+  function onSubmitEdit(editedTodo) {//2
+
+    const newTodos = todos.map(todo => {
+      if (editedTodo.id === todo.id) {
+        return { ...todo, name: editedTodo.name };
+      }
+      return todo;
+    });
+    setTodos(newTodos);
+    setEdit(false);
+    setEditTodo({});
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoList todos={todos} onDelete={onDelete} onEdit={onEdit} />
+      { edit ? 
+        <EditTodo item={editTodo} onSubmitEdit={onSubmitEdit} onCancel={() => setEdit(false)} /> 
+      : <AddTodo onSubmit={onSubmit} /> }
+  </div>
   );
 }
-
 export default App;
